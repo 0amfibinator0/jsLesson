a0feed72a3b57f2288ee52778ef8f441755fe0b0
0amfibinator0 <00amfibiantor00@mail.ru>
2021-10-01 19:16:17 +0300

    обычное задание сделано

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let title = 'box';
-const screens = 'img';
-let screenPrice = 'imgPrice';
-const rollback = 10;
-let fullPrice = 15;
-const adaptive = '76';
+let title = 'jsLesson';
+const screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 560;
+const rollback = 50;
+let fullPrice = 100000;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+console.log(screens.toLowerCase().split(','));
+console.log(fullPrice * (rollback/100) + '%');
+
 
 console.log('сообщение в консол');
 
